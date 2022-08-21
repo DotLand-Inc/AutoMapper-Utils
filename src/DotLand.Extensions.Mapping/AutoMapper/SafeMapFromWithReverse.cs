e7fe1353ceb7006d54a2709f76847c5359f6efc3
Hamza SAII <hamza.saii@outlook.com>
2022-08-21 18:09:31 +0200

    update configuration

--- src/DotLand.Extensions.Mapping/AutoMapper/SafeMapFromWithReverse.cs
+++ src/DotLand.Extensions.Mapping/AutoMapper/SafeMapFromWithReverse.cs
@@ -2,8 +2,20 @@
 
 namespace DotLand.Extensions.Mapping.AutoMapper
 {
+    /// <summary>
+    /// Used to map current class properties with from/to TSource Model ignoring in-existent
+    /// properties
+    /// </summary>
+    /// <typeparam name="TSource">
+    /// Model properties source
+    /// </typeparam>
     public abstract class SafeMapFromWithReverse<TSource>
     {
+        /// <summary>
+        /// Will be executed when project start to configure mapping
+        /// </summary>
+        /// <param name="profile">Current used profile</param>
+        /// <remarks>You can override this function to customise mapping</remarks>
         public virtual void ConfigureMap(Profile profile) =>
             profile.CreateMap(typeof(TSource), GetType())
                 .IgnoreAllNonExistent(typeof(TSource), GetType())
